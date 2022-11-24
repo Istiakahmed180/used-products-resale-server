2b3e7db4711cf544a7a939ef64e23e44bdf8d23c
Istiakahmed180 <istiakahmed180@gmail.com>
2022-11-24 12:30:48 +0600

    caategories product find one by id added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pwqsejd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -21,9 +21,18 @@ async function run() {
       .db("categoryProducts")
       .collection("category");
 
+    //   All categories
     app.get("/categories", async (req, res) => {
       const query = {};
       const result = await categoriesProductsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Categories Products id get
+    app.get("/categories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await categoriesProductsCollection.findOne(query);
       res.send(result);
     });
   } finally {
