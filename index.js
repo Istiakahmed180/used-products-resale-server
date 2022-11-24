ad62173657e49c7fcb79d256a869af0915ca6a31
Istiakahmed180 <istiakahmed180@gmail.com>
2022-11-24 11:22:58 +0600

    monodb database connected on server

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pwqsejd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
 app.get("/", (req, res) => {
   res.send("Final Project Server Is Running");
 });
